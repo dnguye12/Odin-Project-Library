7ad96cbdd991a5bd290efd2d1db942e72bdcb556
duc-huy.nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2022-08-30 00:00:30 +0200

    added some basic animations

--- script.js
+++ script.js
@@ -15,3 +15,36 @@ function Book(title, author, pages, read) {
         console.log(`${this.name} by ${this.author}, ${this.pages} pages, ${red}`);
     }
 };
+
+let BtnAdd = document.getElementById("BtnAdd");
+let overlay = document.getElementById("overlay");
+let add = document.getElementById("add");
+
+overlay.addEventListener("click", function () {
+
+    if (overlay.classList.contains("overlay-active")) {
+
+        overlay.classList.toggle("overlay-active");
+        add.classList.toggle("add-active");
+
+    }
+
+});
+
+BtnAdd.addEventListener("click", function () {
+
+    overlay.classList.toggle("overlay-active");
+    add.classList.toggle("add-active");
+
+});
+
+let form = document.querySelector("form");
+let ftitle = document.getElementById("ftitle");
+let fauthor = document.getElementById("fauthor");
+let fpages = document.getElementById("fpages");
+let red = document.getElementById("checkbox");
+form.addEventListener("submit", function () {
+
+    let helper = new Book(ftitle.textContent, fauthor.textContent, fpages.textContent, red.checked);
+    helper.info();
+});
