c0625fe8941a93472a2176d1c8648d83148cd895
duc-huy.nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2022-08-31 15:27:39 +0200

    javascript done

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function Book(title, author, pages, read) {
         } else {
             red = "not read yet";
         }
-        console.log(`${this.name} by ${this.author}, ${this.pages} pages, ${red}`);
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${red}`);
     }
 };
 
@@ -23,36 +23,91 @@ let add = document.getElementById("add");
 let overlayShown = false;
 BtnAdd.addEventListener("click", function () {
 
-   if( !overlayShown) {
-    overlay.style.visibility = "visible";
-    add.style.visibility = "visible";
-    overlay.style.opacity = "1";
-    add.style.opacity = "1";
-    add.style.transform = "translate(-50%, -50%)";
-    overlayShown = true;
-   }
-});
-
-overlay.addEventListener("click", function() {
-    if(overlayShown) {
-        overlay.style.visibility = "hidden";
-    add.style.visibility = "hidden";
-    overlay.style.opacity = "0";
-    add.style.opacity = "0";
-    add.style.transform = "translate(-500%, -50%)";
-        overlayShown = false;
+    if (!overlayShown) {
+        overlay.style.visibility = "visible";
+        add.style.visibility = "visible";
+        overlay.style.opacity = "1";
+        add.style.opacity = "1";
+        add.style.transform = "translate(-50%, -50%)";
+        overlayShown = true;
     }
 });
+
+function submitted() {
+    if (overlayShown) {
+        ftitle.value = "";
+        fauthor.value = "";
+        fpages.value = "";
+        overlay.style.opacity = "0";
+        add.style.opacity = "0";
+        add.style.transform = "translate(-500%, -50%)";
+        overlayShown = false;
+        overlay.style.visibility = "hidden";
+        add.style.visibility = "hidden";
+    }
+}
+
+overlay.addEventListener("click", submitted());
 
 let form = document.querySelector("form");
 let ftitle = document.getElementById("ftitle");
 let fauthor = document.getElementById("fauthor");
 let fpages = document.getElementById("fpages");
-let red = document.getElementById("checkbox");
+let red = document.getElementById("fread");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     let helper = new Book(ftitle.value, fauthor.value, fpages.value, red.checked);
-    helper.info();
-    library.append(helper);
-    console.log(library.length);
+    library.push(helper);
+    submitted();
+    console.log(library);
+    createLibItem(helper);
 });
+
+
+
+let lib = document.getElementById("lib");
+
+function createLibItem(book) {
+    book.info();
+    let item = document.createElement("div");
+    item.classList.add("item");
+
+    let title = document.createElement("p");
+    title.textContent = book.title;
+    item.appendChild(title);
+    let author = document.createElement("p");
+    author.textContent = book.author;
+    item.appendChild(author);
+    let pages = document.createElement("p");
+    pages.textContent = book.pages;
+    item.appendChild(pages);
+
+    let btnReadStatus = document.createElement("button");
+    if(book.read) {
+        btnReadStatus.classList.add("btnRead");
+        btnReadStatus.textContent = "Read";
+    }else {
+        btnReadStatus.classList.add("btnNotRead");
+        btnReadStatus.textContent = "Not read";
+    }
+    btnReadStatus.addEventListener("click", function() {
+        btnReadStatus.classList.toggle("btnRead");
+        btnReadStatus.classList.toggle("btnNotRead");
+        if(btnReadStatus.textContent == "Read") {
+            btnReadStatus.textContent = "Not read";
+        }else {
+            btnReadStatus.textContent = "Read";
+        }
+    });
+    item.appendChild(btnReadStatus);
+
+    let btnRemove = document.createElement("button");
+    btnRemove.classList.add("btnRemove");
+    btnRemove.textContent = "Remove";
+    btnRemove.addEventListener("click", function() {
+        item.remove();
+    });
+    item.appendChild(btnRemove);
+
+    lib.appendChild(item);
+}
